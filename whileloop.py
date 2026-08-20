08a443f96aa9a4d69f7723cb101675768b4333e8
SIVAPRAKASAN-S <ssivaprakasan317@gmail.com>
2026-08-20 19:52:18 +0530

    second task

---
+++ whileloop.py
@@ -0,0 +1,48 @@
+# multipilication using while loop
+
+
+# n=int(input('enter n'))
+# i=1
+# while i<=40:
+#     print(n,'*',i,'=',n*i)
+#     i+=1    
+
+# Write a program to find the sum of digits of a given number.
+# num = int(input("Enter a number: "))
+# sum_of_digits = 0
+# while num > 0:
+#     digit = num % 10
+#     sum_of_digits += digit
+#     num //= 10
+# print("Sum of digits:", sum_of_digits)
+
+#  Write a program to count the number of digits in a given number.
+
+# num = int(input("Enter a number: "))
+# count = 0
+# while num > 0:
+#     num //= 10
+#     count += 1
+# print("Number of digits:", count)
+
+# Write a program to check whether a given number is an Armstrong number.
+# An Armstrong number is a number that is equal to the sum of its digits each raised to the power of the number of digits.
+# n = int(input("Enter a number: "))
+# num = n
+# count = 0
+# temp = n
+# while temp > 0:
+#     temp //= 10
+#     count += 1
+# sum_of_powers = 0
+# temp = n
+# while temp > 0:
+#     digit = temp % 10
+#     sum_of_powers += digit ** count
+#     temp //= 10
+# if sum_of_powers == n:
+#     print("Armstrong number")
+# else:
+#     print("Not an Armstrong number")
+
+
