d2e5d1b153c9a8bcf30945512f642145e86c5b52
SIVAPRAKASAN-S <ssivaprakasan317@gmail.com>
2026-08-24 20:03:04 +0530

    third task

---
+++ sequence.py
@@ -0,0 +1,15 @@
+
+
+n = int(input("Enter a number: "))
+
+if n <= 1:
+    print("Not Prime")
+else:
+    i = 2
+    while i < n:
+        if n % i == 0:
+            print("Not Prime")
+            break
+        i = i + 1
+    else:
+        print("Prime")
