8a40a4f85239bd8759aa26c839a310439d30b55d
SIVAPRAKASAN-S <ssivaprakasan317@gmail.com>
2026-08-31 20:01:58 +0530

    fourth commit

---
+++ test.py
@@ -0,0 +1,30 @@
+# write a python program to find the given number is prime are not prime?
+
+# n=int(input('Enter a number'))
+# count=0
+# for i in range(1,n+1):
+#     if n%i==0:
+#         count=count+1
+
+# if count==2:
+#     print('prime number')
+# else:
+#     print('not prime number')
+
+
+# # write a python program to find the given number is palindrame or not
+
+# n=int(input('enter a number :'))
+# temp=n
+# reverse=0
+# while n > 0:
+#     digit = n % 10
+#     reverse = reverse * 10 + digit
+#     n= n // 10 
+# if temp == reverse:
+#     print('is number is palindrome')
+# else:
+#     print('is number is not palindrome')
+
+
+
