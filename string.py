53d76d7342f510fa0a63cb0b1139366c5bab373c
SIVAPRAKASAN-S <ssivaprakasan317@gmail.com>
2026-09-01 19:57:44 +0530

    fifth commit

---
+++ string.py
@@ -0,0 +1,55 @@
+# str1="sivaprakasan S"
+# print(str1[:12])
+
+# str1="sivaprakasan S"
+# print(str1[2:12])
+
+# str1="sivaprakasan S"
+# print(str1[6:])
+
+
+# string concatenation
+
+# str1="sivaprakasan S"
+# str2=" is a good boy"
+# str3=str1+str2
+# print(str3)
+
+
+# string append
+# str1="sivaprakasan S"
+# str1 +=" is a good boy"  
+# print(str1) 
+
+# string multiplication
+# str1="sivaprakasan S"
+# str2=str1*6
+# print(str2)
+
+# # string slicing
+str1="sivaprakasan is a good boy"
+# print(str1[:9])
+# print(str1[2:9])
+# print(str1[6:9])    
+
+# print(str1[6:7:2])
+# print(str1[0:11:3])
+# print(str1[::4])
+# print(str1[::-1])
+
+# for loop 
+
+# str="python programming"
+# index=0
+# for i in str:
+#     print(str[:index])
+#     index += 1
+
+
+# formatting string
+
+name = 'siva'
+marks = 90
+# print('name:%s marks:%d' % (name, marks))
+# print('my name is {} and my marks is {}'.format(name, marks))
+# print(f'name: {name} and marks :{marks}')
